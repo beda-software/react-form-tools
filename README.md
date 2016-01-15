c45d24697732eb0db1e7efb0129dfaf78c23ab7e
ruscoder <rusmiligamer@gmail.com>
2016-01-15 12:21:58 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# react-form-tools
-ValidationMixin and base form components for React+Baobab
+[![Build Status](https://travis-ci.org/Brogency/react-form-tools.svg)](https://travis-ci.org/Brogency/react-form-tools)
+
+react-form-tools
+=========
+
+ValidationMixin and base form components for React+Baobab. 
+Based on [baobab-react-schemabranchmixin](https://github.com/Brogency/baobab-react-schemabranchmixin) and [Baobab cursors](https://github.com/Yomguithereal/baobab).
+
+ValidationMixin inspired by [react-validation-mixin](https://github.com/jurassix/react-validation-mixin).
+
+## Installation
+
+  npm install react-form-tools --save
+  
+## Tests
+
+  npm test
+  
+## Release History
+
+* 1.0.0 Initial release
---
+++ karma.conf.js
@@ -0,0 +1,58 @@
+var babelOptions = {
+  presets: ['es2015', 'react']
+};
+
+module.exports = function (config) {
+  config.set({
+    basePath: './',
+    frameworks: ['phantomjs-shim', 'mocha', 'chai'],
+
+    files: [
+      'test/*.spec.js'
+    ],
+    preprocessors: {
+      'test/*.spec.js': 'webpack'
+    },
+
+    reporters: ['coverage', 'dots'],
+
+    webpack: {
+      babel: babelOptions,
+      isparta: {
+        embedSource: true,
+        noAutoWrap: true,
+        // these babel options will be passed only to isparta and not to babel-loader
+        babel: babelOptions
+      },
+      module: {
+        preLoaders: [
+          // Transpile only tests
+          {
+            test: /\.spec\.jsx?/,
+            loader: 'babel',
+            exclude: /node_modules/
+          },
+          // Transpile all project without tests
+          {
+            test: /\.jsx?$/,
+            loader: 'isparta',
+            exclude: [
+              /node_modules/,
+              /\.spec\.jsx?/
+            ]
+          }
+        ]
+      }
+    },
+    webpackServer: {
+      noInfo: true
+    },
+
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['PhantomJS'],
+    autoWatch: true,
+    singleRun: false
+  });
+};
