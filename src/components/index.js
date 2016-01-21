ffd573cc6adf7f39ebd478bc929cd0c17b7b98d7
ruscoder <rusmiligamer@gmail.com>
2016-01-21 17:56:17 +0700

    Fixed import

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export {default as Input} from 'input';
-export {default as Radio} from 'radio';
-export {default as ReInput} from 're-input';
-export {default as Submit} from 'submit';
-export {default as ValidationBox} from 'validation-box';
-export {default as ValidationGlobalError} from 'validation-global-error';
-export {default as Form} from 'form';
+export {default as Input} from './input';
+export {default as Radio} from './radio';
+export {default as ReInput} from './re-input';
+export {default as Submit} from './submit';
+export {default as ValidationBox} from './validation-box';
+export {default as ValidationGlobalError} from './validation-global-error';
+export {default as Form} from './form';
