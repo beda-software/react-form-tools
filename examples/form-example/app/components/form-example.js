04eb9f06192e54ae7ec2b5186e48f593f6e62b8e
ruscoder <rusmiligamer@gmail.com>
2016-05-18 13:49:33 +0700

    Add examples for nested forms

--- examples/form-example/app/components/form-example.js
+++ examples/form-example/app/components/form-example.js
@@ -1,26 +1,85 @@
 import React from 'react';
 import yup from 'yup';
 import SchemaBranchMixin from 'baobab-react-schemabranchmixin';
-import {Form, ValidationBox, Input, Radio, CheckBox, Submit} from 'react-form-tools';
+import {
+    Form,
+    ValidationBox,
+    Input,
+    Radio,
+    CheckBox,
+    Submit
+} from 'react-form-tools';
+
+const NestedForm = React.createClass({
+    displayName: 'NestedForm',
+
+    mixins: [SchemaBranchMixin],
+
+    schema: {
+        form: {},
+    },
+
+    validationSchema: yup.object().shape({}),
+
+    onSubmit(data) {
+        console.log('NestedForm is submitted with valid data', data);
+    },
+
+    onInvalidSubmit(errors) {
+        console.log('NestedForm is submitted with errors', errors);
+    },
+
+    render() {
+        return (
+            <Form
+                style={{ border: '1px solid black', padding: 5, marginTop: 5, marginBottom: 5 }}
+                cursor={this.cursors.form}
+                validationSchema={this.validationSchema}
+                onSubmit={this.onSubmit}
+                onInvalidSubmit={this.onInvalidSubmit}>
+                Nested form
+                <div>
+                    <span>Text Input</span>
+                    <ValidationBox fieldPath="fieldTextInput">
+                        <Input />
+                    </ValidationBox>
+                </div>
+
+                <div>
+                    <Submit>Submit</Submit>
+                </div>
+            </Form>
+        );
+    },
+});
 
 export default React.createClass({
     mixins: [SchemaBranchMixin],
 
     schema: {
+        nestedForm: {},
         form: {},
         formState: {},
     },
 
-    validationSchema: yup.object().shape({
+    validationSchema: yup.object().shape({}),
 
-    }),
+    onSubmit(data) {
+        console.log('Form is submitted with valid data', data);
+    },
+
+    onInvalidSubmit(errors) {
+        console.log('Form is submitted with errors', errors);
+    },
 
     render() {
         return (
             <Form
                 cursor={this.cursors.form}
                 formStateCursor={this.cursors.formState}
-                validationSchema={this.validationSchema}>
+                validationSchema={this.validationSchema}
+                onSubmit={this.onSubmit}
+                onInvalidSubmit={this.onInvalidSubmit}>
                 <div>
                     <span>Text Input</span>
                     <ValidationBox fieldPath="fieldTextInput">
@@ -54,6 +113,11 @@ export default React.createClass({
                         <CheckBox value={true} /> Agree
                     </ValidationBox>
                 </div>
+
+                <div>
+                    <NestedForm tree={this.cursors.nestedForm} />
+                </div>
+
                 <div>
                     <Submit>Submit</Submit>
                 </div>
